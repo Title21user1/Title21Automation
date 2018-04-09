67148148efd3656a3fadbad7319a279336b1786c
Aman Gupta <aman.gupta@neosofttech.com>
2018-04-09 21:22:39 +0530

    Sanity Test Added

--- src/test/java/org/title21/POM/DashBord_POM.java
+++ src/test/java/org/title21/POM/DashBord_POM.java
@@ -12,27 +12,96 @@ public class DashBord_POM
 	BaseClass b=new BaseClass();
 	protected WebDriver driver;
 	
+	public DashBord_POM(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
 	@FindBy(css="#Dashboard>a>span")
 	WebElement dashboard;
 	
 	@FindBy(css=".t21-grid-header-style" )
 	WebElement headerstyle;
 			
-	public DashBord_POM(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-	}
+	@FindBy(xpath="//a[@title='CIBMTR']")
+	WebElement CIBMTR;
 
+	@FindBy(xpath="//li[@id='NavQueries']")
+	WebElement queries;
+	
+	@FindBy(xpath="//li[@id='NavReports']")
+	WebElement reports;
+	
+	@FindBy(xpath="//li[@id='NavCharts']")
+	WebElement charts;
+	
+	@FindBy(xpath="//a[@title='Training']")
+	WebElement LMS;
+	
+	@FindBy(xpath="//li[@id='PlanDevelopment']")
+	WebElement trainingplans;
+	
+	@FindBy(xpath="//li[@id='CourseDevelopment']")
+	WebElement trainingcourses;
+	
+	@FindBy(xpath="//li[@id='CourseReadyToSchedule']")
+	WebElement schedulecourses;
+	
+	@FindBy(xpath="//li[@id='ScheduledCourses']")
+	WebElement schedulelist;
+	
+	@FindBy(xpath="//li[@id='NeedsAssesments']")
+	WebElement scheduletrainee;
+	
 	public WebElement toDashboard()
 	{
 		return dashboard;
 	}
 	
-	/*
-	 * 
-	 * This method verify text on dashboard - (Dashboard). 
-	 * @param WebDriver obj
-	 */
+	public WebElement toCharts()
+	{
+		return charts;
+	}
+	
+	public WebElement toCIBMTR()
+	{
+		return CIBMTR;
+	}
+	
+	public WebElement toQueries()
+	{
+		return queries;
+	}
+	
+	public WebElement toReports()
+	{
+		return reports;
+	}
+	
+	public WebElement toTraningPlans()
+	{
+		return trainingplans;
+	}
+	
+	public WebElement toTraningCourses()
+	{
+		return trainingcourses;
+	}
+	
+	public WebElement toScheduleCourse()
+	{
+		return schedulecourses;
+	}
+	
+	public WebElement toScheduleList()
+	{
+		return schedulelist;
+	}
+	
+	public WebElement toScheduleTrainee()
+	{
+		return scheduletrainee;
+	}
 	
 	public boolean verifyDashboardPrescence(){
 		
@@ -49,11 +118,7 @@ public class DashBord_POM
 		}
 		
 	}
-	
-	/*
-	 * 
-	 * 
-	 */
+
 	
 	public boolean verifyHeaderStyle(){
 		
